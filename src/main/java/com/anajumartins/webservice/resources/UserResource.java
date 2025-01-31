664827a3dbd1a3bef28b5ab46fda241d48b4d0b1
Ana Júlia <aanajulia505@gmail.com>
2025-01-31 18:18:08 -0300

    Configurando database e JPA

---
+++ src/main/java/com/anajumartins/webservice/resources/UserResource.java
@@ -0,0 +1,13 @@
+package com.anajumartins.webservice.resources;
+
+import com.anajumartins.webservice.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+}
