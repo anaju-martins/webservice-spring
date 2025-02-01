52b6f5747e85859f165420a0f98c46ae768e746e
Ana Júlia <aanajulia505@gmail.com>
2025-02-01 19:02:29 -0300

    Entidade Category

--- src/main/java/com/anajumartins/webservice/config/TestConfig.java
+++ src/main/java/com/anajumartins/webservice/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.anajumartins.webservice.config;
 
+import com.anajumartins.webservice.entities.Category;
 import com.anajumartins.webservice.entities.Order;
 import com.anajumartins.webservice.entities.User;
 import com.anajumartins.webservice.entities.enums.OrderStatus;
+import com.anajumartins.webservice.repositories.CategoryRepository;
 import com.anajumartins.webservice.repositories.OrderRepository;
 import com.anajumartins.webservice.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,18 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
@@ -34,6 +46,7 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
     }
 
 
