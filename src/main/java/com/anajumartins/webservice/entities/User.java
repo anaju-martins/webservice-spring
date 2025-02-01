64b5b34eb4c2885808d18870114fa070a0e032ae
Ana Júlia <aanajulia505@gmail.com>
2025-01-31 22:57:59 -0300

    Camada de serviço, registro de componentes, findAll e findById

--- src/main/java/com/anajumartins/webservice/entities/User.java
+++ src/main/java/com/anajumartins/webservice/entities/User.java
@@ -9,8 +9,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 
-@Setter
-@Getter
+
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
@@ -33,6 +32,46 @@ public class User implements Serializable {
         this.name = name;
         this.email = email;
         this.phone = phone;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 
---
+++ src/main/java/com/anajumartins/webservice/services/UserService.java
@@ -0,0 +1,26 @@
+package com.anajumartins.webservice.services;
+
+import com.anajumartins.webservice.entities.User;
+import com.anajumartins.webservice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
