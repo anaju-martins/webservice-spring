3bb089d31ca35f66b3fedb5b63d400e6673bfe7e
Ana Júlia <aanajulia505@gmail.com>
2025-02-06 13:22:15 -0300

    Tratamento de exceção delete

--- src/main/java/com/anajumartins/webservice/services/UserService.java
+++ src/main/java/com/anajumartins/webservice/services/UserService.java
@@ -2,8 +2,11 @@ package com.anajumartins.webservice.services;
 
 import com.anajumartins.webservice.entities.User;
 import com.anajumartins.webservice.repositories.UserRepository;
+import com.anajumartins.webservice.services.exceptions.DatabaseException;
 import com.anajumartins.webservice.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,14 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id, User obj){
